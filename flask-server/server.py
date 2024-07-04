39a0292e944daa87bcd4295c728a22140e5c5747
nbisek <nadiabisek02@gmail.com>
2024-07-04 15:18:42 -0400

    added bsci functions to read excel files

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,14 +1,24 @@
 #./.venv/Scripts/activate
 # python -m venv .venv
+# python server.py
 
 from flask import Flask
+from testing import getAvgs, cleanAvg
+import pandas as pd
 
 app = Flask(__name__)
+
+# Reading the excel files
+xls = pd.ExcelFile('./data/data.xlsx')
+df_avg = pd.read_excel(xls, sheet_name='Admission Avg')
+
+# Cleaning the excel files
+df_avg = cleanAvg(df_avg)
 
 # Members API Route
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    return {"message": "head"}
 
 if __name__ == "__main__":
     app.run(debug=True)
